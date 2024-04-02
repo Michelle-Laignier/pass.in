b4cc1069ee562a49ce092d59ceffd82b790203c9
Laignier <michellelaignier17@gmail.com>
2024-04-01 23:47:06 -0300

    first commit - styles

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import logo from "../../src/assets/logo.svg"
+
+export function Header() {
+  return(
+    <div className="flex items-center gap-5 py-2">
+      <img src={logo} alt="Logo do pass.in" />
+
+      <nav className="flex items-center gap-5">
+        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
+        <a href="" className="font-medium text-sm">Participantes</a>
+      </nav>
+    </div>
+  )
+}
